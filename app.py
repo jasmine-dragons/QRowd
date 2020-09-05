b176ac37e4bc3f8fb472a32a026b39e4a0f9cf82
aidenszeto <aszeto35@gmail.com>
2020-09-05 10:36:33 -0700

    sessions

--- app.py
+++ app.py
@@ -1,11 +1,95 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
+app.secret_key = b'\xa7\xaa(G=\xed\xc6\x7f\xc0\xdf\x93\x0c,P\x11\xf6\xb9I\x8b\r\xff3\xf7\xa5'
+
+URI = ''
+cipher = aes.AESCipher('E14mesLe679V90kN')
 
 
 @app.route('/')
 def index():
-    return render_template();
+    return render_template()
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html', logged_in=session.get('logged_in'));
+
+
+@app.route('/signup_success')
+def signup_success():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    password = request.args.get('password')
+
+    enc_pass = cipher.encrypt(password)
+
+    inputs = {
+        'name': name,
+        'email': email,
+        'password': enc_pass
+    }
+
+    def check_db(email):
+        my_client = pymongo.MongoClient(URI)
+        my_db = my_client['VHomes']
+        my_col = my_db['users']
+        user = my_col.find_one({'email': str(email)})
+        return user
+
+    def add_to_db(inputs):
+        my_client = pymongo.MongoClient(URI)
+        my_db = my_client['VHomes']
+        my_col = my_db['users']
+        my_col.insert_one(inputs)
+
+    if check_db(email) == None:
+        add_to_db(inputs)
+        response = 'sign up successful!'
+        session['logged_in'] = True
+        return render_template('signup_success.html', response=response)
+    else:
+        response = 'email already registered'
+        session['logged_in'] = False
+        return render_template('signup_unsuccessful.html', response=response)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html', logged_in=session.get('logged_in'))
+
+
+@app.route('/login_success')
+def login_success():
+    email = request.args.get('email')
+    password = request.args.get('password')
+
+    inputs = {
+        'email': email,
+        'password': password
+    }
+
+    def get_from_db(email, password):
+        my_client = pymongo.MongoClient(URI)
+        my_db = my_client['VHomes']
+        my_col = my_db['users']
+        user = my_col.find_one({'email': str(email)})
+        dec_pass = cipher.decrypt(str(user['password']))
+        if str(user['email']) == email and dec_pass == password:
+            return True
+        else:
+            return False
+
+    if get_from_db(email, password) == False:
+        response = 'incorrect username or password'
+        session['logged_in'] = False
+        return render_template('login_unsuccessful.html', response=response)
+    else:
+        response = 'logged in as ' + email
+        session['logged_in'] = True
+        return render_template('login_success.html', response=response)
 
 if __name__ == '__main__':
     app.run()
