21a44818486c499453d22b1fd49e5b9b7fcaea28
Pranav Daggubati <pranavdaggubati@gmail.com>
2020-09-05 20:22:12 -0700

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,7 @@ import geocoder
 import json
 from json import loads
 import os
+import requests
 
 load_dotenv()
 MAPS_TOKEN = os.getenv('MAPS_TOKEN')
@@ -29,10 +30,10 @@ except:
 if not js:
     print('error')
 # 'https://api.mapbox.com/datasets/v1/nishantbalaji/ckeq2oto816oe28k48h74h0h8?access_token=' + MAPS_TOKEN
-print(js)
+#print(js)
 
 
-feature_id = 1;
+feature_id = 1
 type = 'Point'
 body = {
   "id": "01",
@@ -54,17 +55,32 @@ body = {
   }
 }
 
-bodyjs = json.dumps(body)
 
-conn.request('PUT', '/datasets/v1/' + MAPS_USER + '/' + DATASET_ID + '/features/' + str(feature_id) + '?access_token=' + MAPS_TOKEN,body=bodyjs)
-response = conn.getresponse()
-print(response.status, response.reason)
+createurl = "https://api.mapbox.com/datasets/v1/pdaggubati?access_token=sk.eyJ1IjoicGRhZ2d1YmF0aSIsImEiOiJja2VxaGJsNjIxYXMzMnJvOXQydWFmbXVlIn0.QsUGLbFfcjNdd387-lCGRA"
+resp = requests.get(createurl)
+print(resp.json())
+print("\n")
+req={
+  "name": "foo123",
+  "description": "bar123"
+}
+#reqjosn = json.dumps(req)
+#resp = requests.post(requrl, data=reqjosn)
+#print(resp.json())
+
+geturl = "https://api.mapbox.com/datasets/v1/pdaggubati/ckeqhoa9g25dw22qb3vlzcu4l?access_token=sk.eyJ1IjoicGRhZ2d1YmF0aSIsImEiOiJja2VxaGJsNjIxYXMzMnJvOXQydWFmbXVlIn0.QsUGLbFfcjNdd387-lCGRA"
+resp = requests.get(geturl)
+print(resp.json())
+print("\n")
+
+featurl = "https://api.mapbox.com//datasets/v1/pdaggubati/ckeqhoa9g25dw22qb3vlzcu4l/features?access_token=sk.eyJ1IjoicGRhZ2d1YmF0aSIsImEiOiJja2VxaGJsNjIxYXMzMnJvOXQydWFmbXVlIn0.QsUGLbFfcjNdd387-lCGRA"
+resp = requests.get(featurl)
+print(resp.json())
+print("\n")
 
 
-#conn.request('GET', '/datasets/v1/' + MAPS_USER + '/' + DATASET_ID + '/features?access_token=' + MAPS_TOKEN)
-# Decode UTF8 responses
-#res = conn.getresponse()
-#data = res.read().decode("utf-8")
-# Create JSON object from data
-#js = json.loads(data)
-#print(js)
+
+'''posturl = "https://api.mapbox.com/datasets/v1/pdaggubati/ckeqhoa9g25dw22qb3vlzcu4l/features/1?access_token=sk.eyJ1IjoicGRhZ2d1YmF0aSIsImEiOiJja2VxaGJsNjIxYXMzMnJvOXQydWFmbXVlIn0.QsUGLbFfcjNdd387-lCGRA"
+resp = requests.post(posturl, data=body)
+print(resp.json())
+'''
